0b0a9bf8c3e9372a41212e910bf33fff2321a1dd
wyhw <wanyanhw@lanxi.com>
2020-05-26 16:14:42 +0800

    ip工具包

--- src/main/java/com/wyhw/pmp/util/IpUtils.java
+++ src/main/java/com/wyhw/pmp/util/IpUtils.java
@@ -5,6 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author wanyanhw
@@ -17,39 +21,52 @@ public class IpUtils {
         String unknown = "unknown";
         // ip地址分割符号
         String ipSplitChar = ",";
+        // 本机Ip
+        Set<String> localhostIps = new HashSet<>();
+        localhostIps.add("0:0:0:0:0:0:0:1");
+        localhostIps.add("127.0.0.1");
 
         String ip = request.getHeader("x-Forward-For");
         if (!StringUtils.isEmpty(ip) && !unknown.equalsIgnoreCase(ip)) {
             if (ip.contains(ipSplitChar)) {
                 ip = ip.split(ipSplitChar)[0];
             }
-            log.info("x-Forward-For Ip : {{}}", ip);
+            log.info("x-Forward-For Ip : {}", ip);
         }
         if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
             ip = request.getHeader("Proxy-Client-IP");
-            log.info("Proxy-Client-IP Ip : {{}}", ip);
+            log.info("Proxy-Client-IP Ip : {}", ip);
         }
         if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
             ip = request.getHeader("WL-Proxy-Client-IP");
-            log.info("WL-Proxy-Client-IP Ip : {{}}", ip);
+            log.info("WL-Proxy-Client-IP Ip : {}", ip);
         }
         if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_CLIENT_IP");
-            log.info("HTTP_CLIENT_IP Ip : {{}}", ip);
+            log.info("HTTP_CLIENT_IP Ip : {}", ip);
         }
         if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
             ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-            log.info("HTTP_X_FORWARDED_FOR Ip : {{}}", ip);
+            log.info("HTTP_X_FORWARDED_FOR Ip : {}", ip);
         }
         if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
             ip = request.getHeader("X-Real-IP");
-            log.info("X-Real-IP Ip : {{}}", ip);
+            log.info("X-Real-IP Ip : {}", ip);
         }
         if (StringUtils.isEmpty(ip) || unknown.equalsIgnoreCase(ip)) {
             ip = request.getRemoteAddr();
-            log.info("getRemoteAddr Ip : {{}}", ip);
+            log.info("getRemoteAddr Ip : {}", ip);
         }
-        log.info("客户端Ip : {{}}", ip);
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            if (localhostIps.contains(ip)) {
+                ip = inetAddress.getHostAddress();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        log.info("客户端Ip : {}", ip);
         return ip;
     }
 }
