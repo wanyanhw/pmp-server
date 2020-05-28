966aba71e0929f5c9d061b4916f6ae5cc7378ac7
wyhw <wanyanhw@lanxi.com>
2020-05-28 18:03:12 +0800

    GPS服务

---
+++ src/main/java/com/wyhw/pmp/gps/service/GpsService.java
@@ -0,0 +1,9 @@
+package com.wyhw.pmp.gps.service;
+
+/**
+ * 位置服务接口
+ * @author wanyanhw
+ * @date 2020/5/28 14:12
+ */
+public interface GpsService {
+}
---
+++ src/main/java/com/wyhw/pmp/gps/service/impl/GpsServiceImpl.java
@@ -0,0 +1,12 @@
+package com.wyhw.pmp.gps.service.impl;
+
+import com.wyhw.pmp.gps.service.GpsService;
+import org.springframework.stereotype.Service;
+/**
+ * @author wanyanhw
+ * @date 2020/5/28 14:13
+ */
+@Service
+public class GpsServiceImpl implements GpsService {
+
+}
