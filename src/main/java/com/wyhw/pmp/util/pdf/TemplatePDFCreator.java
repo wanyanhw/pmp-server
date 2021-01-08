b695f587adb72d217d52adc304670ea948e71bcd
wanyanhw <18846817897@163.com>
2021-01-08 18:01:36 +0800

    PDF模板生成器

--- src/main/java/com/wyhw/pmp/util/pdf/TemplatePDFCreator.java
+++ src/main/java/com/wyhw/pmp/util/pdf/TemplatePDFCreator.java
@@ -1,22 +1,46 @@
 package com.wyhw.pmp.util.pdf;
 
+import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfStamper;
+import org.apache.catalina.core.ApplicationContext;
 
-import java.io.IOException;
+import javax.servlet.ServletContext;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.Map;
 
 /**
  * 模板生成pdf
  */
 public class TemplatePDFCreator {
 
-    public OutputStream build(String path, String fileName, String[] args) {
-        PdfReader reader = null;
+    public OutputStream build(String path, String fileName, Map<String, String> argMap) throws Exception {
+        PdfReader pdfReader = null;
+        PdfStamper pdfStamper = null;
         try {
-            reader = new PdfReader(path + fileName);
+            pdfReader = new PdfReader(path + fileName);
 
-        } catch (IOException e) {
+            String root = "";
+
+            File outFile = new File(root);
+            pdfStamper = new PdfStamper(pdfReader, new FileOutputStream(outFile));
+
+            AcroFields acroFields = pdfStamper.getAcroFields();
+            for (String  k : argMap.keySet()) {
+                acroFields.setField(k, argMap.get(k));
+            }
+            pdfStamper.setAnnotationFlattening(true);
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (pdfStamper != null) {
+                pdfStamper.close();
+            }
+            if (pdfReader != null) {
+                pdfReader.close();
+            }
         }
         return null;
     }
