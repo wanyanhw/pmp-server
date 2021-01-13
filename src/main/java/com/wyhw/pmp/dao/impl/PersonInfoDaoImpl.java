bc6348fc4f8c48c6862ffa5e1d1fb2872d949227
wanyanhw <18846817897@163.com>
2021-01-13 14:11:53 +0800

    族谱接口创建

---
+++ src/main/java/com/wyhw/pmp/dao/impl/PersonInfoDaoImpl.java
@@ -0,0 +1,26 @@
+package com.wyhw.pmp.dao.impl;
+
+import com.wyhw.pmp.entity.PersonInfoEntity;
+import com.wyhw.pmp.mapper.PersonInfoMapper;
+import com.wyhw.pmp.dao.IPersonInfoDao;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author wanyanhw
+ * @since 2021-01-13
+ */
+@Service
+public class PersonInfoDaoImpl extends ServiceImpl<PersonInfoMapper, PersonInfoEntity> implements IPersonInfoDao {
+
+    @Override
+    public List<PersonInfoEntity> listAllPeople() {
+        return list();
+    }
+}
---
+++ src/main/java/com/wyhw/pmp/mapper/PersonInfoMapper.java
@@ -0,0 +1,16 @@
+package com.wyhw.pmp.mapper;
+
+import com.wyhw.pmp.entity.PersonInfoEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author wanyanhw
+ * @since 2021-01-13
+ */
+public interface PersonInfoMapper extends BaseMapper<PersonInfoEntity> {
+
+}
