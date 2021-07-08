fa3c416571f5066f4fef82e6645dfe8195f97698
wanyanhw <wyhwlml@126.com>
2021-07-08 14:22:26 +0800

    Revert "跨域配置日志清除"
    This reverts commit aba0aee8

--- src/main/java/com/wyhw/pmp/config/CorsFilter.java
+++ src/main/java/com/wyhw/pmp/config/CorsFilter.java
@@ -1,5 +1,8 @@
 package com.wyhw.pmp.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 
@@ -13,13 +16,19 @@ import java.io.IOException;
  * @author wyhw
  * @date 2020-5-26
  */
+@Slf4j
 @WebFilter(urlPatterns = {"/*"}, filterName = "corsFilter")
 public class CorsFilter implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(CorsFilter.class);
+
     @Override
-    public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        logger.info("CorsFilter ...");
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
@@ -29,11 +38,14 @@ public class CorsFilter implements Filter {
         /*options 请求返回允许跨域的头*/
         if (HttpMethod.OPTIONS.name().equalsIgnoreCase(request.getMethod()) &&
                 request.getHeader(HttpHeaders.ORIGIN) != null) {
+            logger.info("doFilter options request");
             return;
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
-    public void destroy() {}
+    public void destroy() {
+
+    }
 }
