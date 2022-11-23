7bfd931996785a13b35e4d1b863753270b8a0746
wanyanhw <wyhwlml@126.com>
2022-11-23 14:06:57 +0800

    聚合扫雷游戏代码

--- src/test/java/com/wyhw/pmp/game/MineClearance.java
+++ src/test/java/com/wyhw/pmp/game/MineClearance.java
@@ -1,8 +1,9 @@
 package com.wyhw.pmp.game;
 
 import javax.swing.*;
-
-import static com.wyhw.pmp.game.MapPanel.PIX_WIDTH;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * @author wanyanhw
@@ -34,22 +35,22 @@ public class MineClearance {
 
     private static final JFrame jFrame = new JFrame();
 
-    public static final int MINE_FLAG = -1;
+    private static final int MINE_FLAG = -1;
 
-    public static final int NO_MINE_NEAR = 0;
+    private static final int NO_MINE_NEAR = 0;
 
     public static void main(String[] args) {
         initMap();
     }
 
-    public static void initMap() {
+    private static void initMap() {
         mineClearanceInstance.loadMineMapData();
         mineClearanceInstance.showFrame();
     }
 
     private void showFrame() {
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.setSize((width + 2) * PIX_WIDTH, (height + 3) * PIX_WIDTH);
+        jFrame.setSize((width + 2) * MapPanel.PIX_WIDTH, (height + 3) * MapPanel.PIX_WIDTH);
         jFrame.setLocation(800, 200);
         jFrame.add(new MapPanel(map), 0);
         jFrame.setVisible(true);
@@ -128,5 +129,89 @@ public class MineClearance {
             return;
         }
         map[indexX][indexY] ++;
+    }
+
+    private static class MapPanel extends JPanel {
+
+        public static final int PIX_WIDTH = 50;
+        private int[][] map;
+        private JButton[][] buttons;
+
+        MapPanel(int[][] map) {
+            this.map = map;
+            buttons = new JButton[map.length][map[0].length];
+            initButtonArea();
+        }
+
+        private void initButtonArea() {
+            this.setLayout(new BorderLayout());
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    final int x = i, y = j;
+                    JButton button = new JButton();
+                    button.setBounds((j + 1) * PIX_WIDTH, (i + 1) * PIX_WIDTH, PIX_WIDTH, PIX_WIDTH);
+                    button.addMouseListener(new MouseAdapter() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
+                            int btn = e.getButton();
+                            if (btn == MouseEvent.BUTTON3) {
+                                // 鼠标右键点击
+                                if (button.getText().equals("L")) {
+                                    resetButton(button);
+                                    return;
+                                }
+                                button.setText("L");
+                                button.setBackground(Color.RED);
+                                return;
+                            }
+                            if (button.getText().equals("L")) {
+                                resetButton(button);
+                            }
+                            button.setText(map[x][y] + "");
+                            if (map[x][y] == MINE_FLAG) {
+                                showAllMine();
+                            }
+                            if (map[x][y] == NO_MINE_NEAR) {
+                                showNear(x, y, map.length, map[0].length);
+                            }
+                        }
+                    });
+                    button.setVisible(true);
+                    buttons[i][j] = button;
+                    this.add(button, BorderLayout.AFTER_LAST_LINE);
+                }
+            }
+            Button startButton = new Button("start");
+            startButton.addActionListener(e -> initMap());
+            this.add(startButton, BorderLayout.AFTER_LAST_LINE);
+        }
+
+        private void resetButton(JButton button) {
+            button.setText("");
+            button.setBackground(MapPanel.super.getParent().getBackground());
+        }
+
+        private void showNear(int x, int y, int maxX, int maxY) {
+            for (int i = 0; i <= 2; i ++) {
+                for (int j = 0; j <= 2; j++) {
+                    int indexX = x + j - 1;
+                    int indexY = y + i - 1;
+                    if (indexX < 0 || indexY < 0 || indexX >= maxX || indexY >= maxY) {
+                        continue;
+                    }
+                    buttons[indexX][indexY].setText(map[indexX][indexY] + "");
+                }
+            }
+        }
+
+        private void showAllMine() {
+            for (int i = 0; i < map.length; i++) {
+                for (int j = 0; j < map[i].length; j++) {
+                    if (map[i][j] == MINE_FLAG) {
+                        buttons[i][j].setText(MINE_FLAG + "");
+                    }
+                }
+            }
+        }
     }
 }
